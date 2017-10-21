b563a52e0002b2f37ad710ee143bc0b2e6169735
Mancho <manchoblogs@gmail.com>
2017-10-21 22:03:48 +0530

    new update

--- wishes/script/user.js
+++ wishes/script/user.js
@@ -7,17 +7,16 @@ $urlParam = function(name){
        return decodeURI(results[1]) || 0;
     }
 	
-<<<<<<< HEAD
-var url = $urlParam(&#39;d&#39;);
-var modifiedUrl = url.replace('&m=1','').replace('','');
-console.log(modifiedUrl);
-=======
 }
 console.log($urlParam('d')); 
 alert($urlParam('d')); 
 
+var url = $urlParam('d');
+var modifiedUrl = url.replace('&m=1','').replace('','');
+console.log(modifiedUrl);
+
 $(document).ready(function(){
-var name = $urlParam('d');
+var name = modifiedUrl;
 $("#username").html(name);
 })
->>>>>>> e565111ee2c217e53f5d4e6630e926b6a2f17e6e
+	
