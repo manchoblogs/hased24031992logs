938ad47c654978eb0716f1751f5186f5521d20f9
Mancho <manchoblogs@gmail.com>
2017-10-21 22:11:56 +0530

    username

---
+++ wishes/script/username.js
@@ -0,0 +1,21 @@
+$urlParam = function(name){
+    var results = new RegExp('[\?]' + name + '=([^#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return decodeURI(results[1]) || 0;
+    }
+	
+}
+console.log($urlParam('u')); 
+
+var url = $urlParam('u');
+$modifiedUrl = url.replace('&m=1','&m=0','&').replace('','','');
+console.log(modifiedUrl);
+
+$(document).ready(function(){
+var name = $modifiedUrl;
+$("#username").html(name);
+})
+	
