e6e34f05b05ea1288bd3dfd2aeadb0679beb8a41
Mancho <manchoblogs@gmail.com>
2017-10-21 23:20:54 +0530

    sendsmsi
    your name

---
+++ wishes/script/ismuka.js
@@ -0,0 +1,21 @@
+$urlParam = function(name){
+    var results = new RegExp('[\?]' + name + '=([^#]*)').exec(window.location.href);
+    if (results==null){
+       return 'Type Your Name';
+    }
+    else{
+       return decodeURI(results[1]) || 0;
+    }
+	
+}
+console.log($urlParam('u')); 
+
+var url = $urlParam('u');
+$modifiedUrl = url.replace('&m=1','').replace('&m=0','').replace('+',' ');
+console.log($modifiedUrl);
+
+jQuery(document).ready(function($){
+var name = $modifiedUrl;
+$("#username").html(name);
+})
+	
