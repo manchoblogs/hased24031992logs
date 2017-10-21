e565111ee2c217e53f5d4e6630e926b6a2f17e6e
manchoblogs <manchoblogs@gmail.com>
2017-10-21 21:52:35 +0530

    Update user.js

--- wishes/script/user.js
+++ wishes/script/user.js
@@ -1,13 +1,17 @@
 $urlParam = function(name){
-    var results = new RegExp(&#39;[\?]&#39; + name + &#39;=([^#]*)&#39;).exec(window.location.href);
+    var results = new RegExp('[\?]' + name + '=([^#]*)').exec(window.location.href);
     if (results==null){
        return null;
-    }else{
-	return decodeURI(results[1]) || 0;
-    }}
-console.log($urlParam(&#39;d&#39;)); 
-	jQuery(document).ready(function($){
-	var name = $urlParam(&#39;d&#39;);
-	$(&quot;#result&quot;).html(name);
-	});
+    }
+    else{
+       return decodeURI(results[1]) || 0;
+    }
 	
+}
+console.log($urlParam('d')); 
+alert($urlParam('d')); 
+
+$(document).ready(function(){
+var name = $urlParam('d');
+$("#username").html(name);
+})
