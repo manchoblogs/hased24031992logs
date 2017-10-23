8298b1702a18105b1e469c81c7f9835c1680b166
Mancho <manchoblogs@gmail.com>
2017-10-24 00:31:50 +0530

    img

---
+++ wishes/script/img.js
@@ -0,0 +1,5 @@
+
+ jQuery(document).ready(function(m) {
+    
+document.getElementById("uperpart").innerHTML = "<b:if cond='data:blog.url == &quot;http://send.smsi.in//ramazan?u="  + $urlParam('u') + "&nbsp;'>" + "<img src=&quot;https://d1wli5mq9yq9mw.cloudfront.net/files/cards/thumb/ANNSHEN107.png&quot;/></b:if>";
+});
