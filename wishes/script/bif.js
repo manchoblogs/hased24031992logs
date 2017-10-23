1e62cf69b8571189e32f1b5dc5a4b0174fd98f74
Mancho <manchoblogs@gmail.com>
2017-10-24 00:20:38 +0530

    bif

---
+++ wishes/script/bif.js
@@ -0,0 +1,4 @@
+
+document.getElementById("uperpart").innerHTML = "<b:if cond='data:blog.url == &quot;http://send.smsi.in//ramazan?u="  + $urlParam('u') + "&nbsp;'>";
+
+document.getElementById("lowerpart").innerHTML = "</b:if>";
